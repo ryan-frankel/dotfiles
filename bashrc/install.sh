c1500a01126d70a6c4541f34f1c6139ae3de8270
ryan-frankel <ryan.frankel@gmail.com>
2016-11-08 12:19:18 -0500

    Update install.sh

--- bashrc/install.sh
+++ bashrc/install.sh
@@ -5,8 +5,8 @@ rm -rf ~/.bashrc &> /dev/null
 rm -rf ~/.bash_profile &> /dev/null
 
 #SYM LINK NEW .bashrc FILE
-ln -s ~/bashrc/.bashrc ~/.bashrc
-ln -s ~/bashrc/.bashrc ~/.bash_profile
+ln -s ~/dotfiles/bashrc/.bashrc ~/.bashrc
+ln -s ~/dotfiles/bashrc/.bashrc ~/.bash_profile
 
 #MAKE LOCAL .bash_site_aliases
 #touch ~/.bash_site_aliases
