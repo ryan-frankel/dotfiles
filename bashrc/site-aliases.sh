7f0f04471a614db8a8b4e5cb6684f744d95f82b5
Ryan Frankel <ryan@digitalbrands.com>
2016-11-28 22:10:57 +0000

    Updated site aliases to have username.

--- bashrc/site-aliases.sh
+++ bashrc/site-aliases.sh
@@ -1,17 +1,17 @@
 # SITES
 # #########################################
-alias bc='mosh 166.78.243.119'
-alias ha='mosh 23.253.111.148'
-alias cr='mosh cardrates.com'
-alias pr='mosh printaholic.com'
-alias da='mosh datingadvice.com'
-alias dc='mosh dealcrunch.com'
-alias rf='mosh rf.passprotect.me'
-alias td='mosh thedrizzle.org'
+alias bc='mosh rfrankel@166.78.243.119'
+alias ha='mosh rfrankel@123.253.111.148'
+alias cr='mosh rfrankel@1cardrates.com'
+alias pr='mosh rfrankel@1printaholic.com'
+alias da='mosh rfrankel@1datingadvice.com'
+alias dc='mosh rfrankel@1dealcrunch.com'
+alias rf='mosh rfrankel@1rf.passprotect.me'
+alias td='mosh rfrankel@1thedrizzle.org'
 
 # MINE
 # #########################################
-alias farm='mosh farmzip.com'
-alias fzwd='cd /home/farm-zip/'
-alias nerdmash='mosh nerdmash.com'
-alias shirts='mosh gainesvilleshirts.com'
+alias farm='mosh rfrankel@1farmzip.com'
+alias fzwd='cd rfrankel@1/home/farm-zip/'
+alias nerdmash='mosh rfrankel@1nerdmash.com'
+alias shirts='mosh rfrankel@1gainesvilleshirts.com'
