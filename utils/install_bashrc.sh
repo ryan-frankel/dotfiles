d92f1d8debb158068b38522911352cb25e1a5cf5
Ryan Frankel <ryan@digitalbrands.com>
2017-04-11 18:19:33 +0000

    Added inputrc and history tracking.

--- utils/install_bashrc.sh
+++ utils/install_bashrc.sh
@@ -5,14 +5,16 @@ function install_bashrc() {
 	ech "- Delete existing..."
 	rm -rf ~/.bashrc &> /dev/null
 	rm -rf ~/.bash_profile &> /dev/null
+  rm -rf ~/.inputrc &> /dev/null
 
 	#SYM LINK NEW .bashrc FILE
 	ech "- Create new symlinks..."
 	ln -s ~/dotfiles/bashrc/bashrc.sh ~/.bashrc
 	ln -s ~/dotfiles/bashrc/bashrc.sh ~/.bash_profile
+  ln -s ~/dotfiles/bashrc/inputrc.sh ~/.inputrc
 
 	#RELOAD .bashrc
-  ech "- Reload bashrc..."
-  source ~/.bashrc
+  #ech "- Reload bashrc..."
+  #source ~/.bashrc
 }
 
