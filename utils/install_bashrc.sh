6ad650ea25d6c82442f885eeef8f431399b09755
Ryan Frankel <ryan@digitalbrands.com>
2017-04-23 16:58:58 +0000

    Adding tmux.

--- utils/install_bashrc.sh
+++ utils/install_bashrc.sh
@@ -6,12 +6,14 @@ function install_bashrc() {
 	rm -rf ~/.bashrc &> /dev/null
 	rm -rf ~/.bash_profile &> /dev/null
   rm -rf ~/.inputrc &> /dev/null
+  rm -rf ~/.tmux.conf &> /dev/null
 
 	#SYM LINK NEW .bashrc FILE
 	ech "- Create new symlinks..."
 	ln -s ~/dotfiles/bashrc/bashrc.sh ~/.bashrc
 	ln -s ~/dotfiles/bashrc/bashrc.sh ~/.bash_profile
   ln -s ~/dotfiles/bashrc/inputrc.sh ~/.inputrc
+  ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 	#RELOAD .bashrc
   #ech "- Reload bashrc..."
