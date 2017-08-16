f9da6e2ad03244bea788ffc5ad22a3def53ec38b
Ryan Frankel <ryan@digitalbrands.com>
2017-08-16 10:26:23 -0400

    Switched to eternal terminal, and added rfd.

--- bashrc/site-aliases.sh
+++ bashrc/site-aliases.sh
@@ -1,18 +1,19 @@
 # SITES
 # #########################################
-alias bc='mosh rfrankel@166.78.243.119'
-alias ha='mosh rfrankel@23.253.111.148'
-alias cr='mosh rfrankel@cardrates.com'
-alias pr='mosh rfrankel@printaholic.com'
-alias da='mosh rfrankel@datingadvice.com'
-alias dc='mosh rfrankel@dealcrunch.com'
-alias rf='mosh rfrankel@rf.passprotect.me'
-alias td='mosh rfrankel@thedrizzle.org'
-alias dbd='mosh rfrankel@23.253.238.205'
+alias bc='et rfrankel@166.78.243.119'
+alias ha='et rfrankel@23.253.111.148'
+alias cr='et rfrankel@cardrates.com'
+alias pr='et rfrankel@printaholic.com'
+alias da='et rfrankel@datingadvice.com'
+alias dc='et rfrankel@dealcrunch.com'
+alias rf='et rfrankel@rf.passprotect.me'
+alias td='et rfrankel@thedrizzle.org'
+alias dbd='et rfrankel@23.253.238.205'
+alias rfd='et rfrankel@165.227.106.79'
 
 # MINE
 # #########################################
-alias farm='mosh rfrankel@farmzip.com'
-alias nerdmash='mosh rfrankel@nerdmash.com'
-alias shirts='mosh rfrankel@gainesvilleshirts.com'
-alias testfyi='mosh rfrankel@test.fyi'
+alias farm='et rfrankel@farmzip.com'
+alias nerdmash='et rfrankel@nerdmash.com'
+alias shirts='et rfrankel@gainesvilleshirts.com'
+alias testfyi='et rfrankel@test.fyi'
