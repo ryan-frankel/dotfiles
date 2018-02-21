f03b30489444fd83366c12f6b4d701c8fcc1a401
Ryan Frankel <ryan@digitalbrands.com>
2018-02-21 15:38:42 -0500

    Added disavow site alias and cleanup.

--- bashrc/site-aliases.sh
+++ bashrc/site-aliases.sh
@@ -2,14 +2,16 @@
 # #########################################
 alias bc='mosh rfrankel@166.78.243.119'
 alias ha='mosh rfrankel@23.253.111.148'
-alias cr='mosh rfrankel@cardrates.com'
-alias pr='mosh rfrankel@printaholic.com'
-alias da='mosh rfrankel@datingadvice.com'
-alias dc='mosh rfrankel@dealcrunch.com'
+alias hassh='ssh rfrankel@23.253.111.148'
+alias cr='mosh rfrankel@166.78.159.169'
+alias pr='mosh rfrankel@166.78.123.101'
+alias da='mosh rfrankel@209.59.129.135'
+alias dc='mosh rfrankel@104.239.165.198'
+alias disavow='mosh rfrankel@165.227.78.247'
 
-alias rf='mosh rfrankel@rf.passprotect.me'
-alias rfssh='ssh rfrankel@rf.passprotect.me'
-alias td='mosh rfrankel@thedrizzle.org'
+alias rf='mosh rfrankel@104.131.21.93'
+alias rfssh='ssh rfrankel@104.131.21.93'
+alias td='mosh rfrankel@159.203.99.208'
 alias dbd='mosh rfrankel@23.253.238.205'
 alias rfd='mosh rfrankel@165.227.106.79'
 
