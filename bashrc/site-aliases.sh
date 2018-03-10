a0e6253b5f42c8ec0917d0e3dcaa1880c6c7c07e
Ryan Frankel <ryan@digitalbrands.com>
2018-03-10 18:49:30 +0000

    Added juiceminer site alias.

--- bashrc/site-aliases.sh
+++ bashrc/site-aliases.sh
@@ -13,6 +13,7 @@ alias qaland='mosh rfrankel@qa.land'
 alias qatown='mosh rfrankel@qa.town'
 alias noodle='mosh rfrankel@noodleball.in'
 alias rainmk='mosh rfrankel@192.241.143.25'
+alias juice='mosh rfrankel@159.65.183.90'
 
 alias rf='mosh rfrankel@159.65.183.90'
 alias rfssh='ssh rfrankel@159.65.183.90'
